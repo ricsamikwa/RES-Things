e45a520b5fd2907946a441de8c2ecdaa6a1bf405
ricsamikwa <esamikwa@gmail.com>
2022-02-13 21:28:04 +0100

    new optimisation format script - minimization

---
+++ optimisation_test.py
@@ -0,0 +1,10 @@
+
+# numpy implementation of argmax
+from numpy import argmax
+from numpy import asarray
+# define vector
+probs = asarray([[0.4, 0.5, 0.1], [0.0, 0.0, 1.0], [0.9, 0.0, 0.1], [0.3, 0.3, 0.4]])
+print(probs.shape)
+# get argmax
+result = argmax(probs, axis=1)
+print(result)
