9bd038f2dd5d8675d248c0c8a28924a9ac3dea0f
ricsamikwa <esamikwa@gmail.com>
2022-02-23 10:23:37 +0100

    resolving merge conflicts on optimisation file

---
+++ optimisation_test2.py
@@ -0,0 +1,44 @@
+
+# numpy implementation of argmax
+from numpy import argmax
+from numpy import asarray
+from numpy import sum
+
+
+def local_layerwise_time():
+    layerwise_latency = [0.020343, 0.033343, 0.023343, 0.012343, 0.03467367, 0.01111876, 0.00213267]
+    return layerwise_latency
+def server_layerwise_time():
+    layerwise_latency = [0.0020343, 0.0033343, 0.0023343, 0.0012343, 0.003467367, 0.001111876, 0.000213267]
+    return layerwise_latency
+    
+def transmission_layerwise_time():
+    layerwise_latency = [0.0033343, 0.0023343, 0.0012343, 0.003467367, 0.001111876, 0.000213267]
+    return layerwise_latency
+
+def training_time(layer):
+    device_layerwise_time = local_layerwise_time()
+    se_layerwise_time = server_layerwise_time()
+    trans_layerwise_time = transmission_layerwise_time()
+    
+    training_time_array = []
+
+    for i in len(device_layerwise_time):
+        training_time_array[i] = sum(device_layerwise_time[0:i]) + sum(se_layerwise_time[i:]) + trans_layerwise_time[i]
+    
+    return training_time_array
+def power_measured():
+    layerwise_power = [0.0033343, 0.0023343, 0.0012343, 0.003467367, 0.001111876, 0.000213267]
+    return layerwise_power
+def energy_consumption():
+    ghsdg
+def combination():
+    ghsdg
+def combination_with_alpha():
+    ghsdg
+# define vector
+probs = asarray([[0.4, 0.5, 0.1], [0.0, 0.0, 1.0], [0.9, 0.0, 0.1], [0.3, 0.3, 0.4]])
+print(probs.shape)
+# get argmax
+result = argmax(probs, axis=1)
+print(result)
